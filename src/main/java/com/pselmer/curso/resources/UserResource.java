c33a40620b30c3f45cd2d1c1626916133103b403
Paulo Selmer <pauloselmer@gmail.com>
2026-05-07 21:18:40 -0300

    service user

--- src/main/java/com/pselmer/curso/resources/UserResource.java
+++ src/main/java/com/pselmer/curso/resources/UserResource.java
@@ -1,18 +1,28 @@
 package com.pselmer.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pselmer.curso.entities.User;
+import com.pselmer.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@ Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Paulo Selmer", "pauloselmer@gmail.com", "43 98402-4660", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+
+		
+		return ResponseEntity.ok().body(list);
 	}
 }
